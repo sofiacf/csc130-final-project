04bcae1010e3ba0902497b157a1c3c12ebadbbf8
GabbyNorth <gaabriele@gmail.com>
2021-05-10 19:30:56 -0400

    implement binary search

--- src/com/csc130/list_search/ListSearcher.java
+++ src/com/csc130/list_search/ListSearcher.java
@@ -3,8 +3,8 @@ package com.csc130.list_search;
 import java.util.List;
 
 public class ListSearcher {
-	private List list;
-	public ListSearcher(List list) {
+	private List<Integer> list;
+	public ListSearcher(List<Integer> list) {
 		this.list = list;
 	}
 
@@ -12,7 +12,20 @@ public class ListSearcher {
 //		TODO: implement this!
 	}
 
-	public binarySearch() {
-//		todo: implement this!
+
+	int binarySearch(int low, int high, int x) {
+		if (high >= low) {
+			int mid = low + (high - low) / 2;
+
+			if (list.get(mid) == x)
+				return mid;
+
+			if (list.get(mid) > x)
+				return binarySearch(low, mid - 1, x);
+
+			return binarySearch(mid + 1, high, x);
+		}
+		return -1;
 	}
+
 }
