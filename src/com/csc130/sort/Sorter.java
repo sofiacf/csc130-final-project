e3a26ef1217d92e4f04190204cada83c2c7c6983
GabbyNorth <gaabriele@gmail.com>
2021-05-10 20:35:51 -0400

    implement insertion sort

--- src/com/csc130/sort/Sorter.java
+++ src/com/csc130/sort/Sorter.java
@@ -3,9 +3,9 @@ package com.csc130.sort;
 import java.util.List;
 
 public class Sorter {
-	private final List list;
 
-	public Sorter(List list) {
+	private List<Integer> list;
+	public Sorter(List<Integer> list) {
 		this.list = list;
 	}
 
@@ -13,8 +13,20 @@ public class Sorter {
 //		todo: implement this!
 	}
 
-	public insertionSort() {
-//		todo: implement this!
+	public void insertionSort() {
+		int n = list.size();
+
+		for (int i = 1; i < n; i++) {
+			Integer temp = list.get(i);
+			int j = i - 1;
+
+			while (j >= 0 && temp < list.get(j)) {
+				list.set(j + 1, list.get(j));
+				j--;
+			}
+
+			list.set(j + 1, temp);
+		}
 	}
 
 	public quickSort() {
