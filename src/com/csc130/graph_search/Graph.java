1dbbaa76b7aeec806cbe2f368766d330ea292de6
Steven Sousa <silver.steven67@gmail.com>
2021-05-10 20:43:59 -0400

    Implement Queue BFS

--- src/com/csc130/graph_search/Graph.java
+++ src/com/csc130/graph_search/Graph.java
@@ -1,5 +1,6 @@
 package com.csc130.graph_search;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Stack;
 import java.util.Vector;
@@ -22,7 +23,7 @@ class Graph {
 		lists[v].add(w);
 	}
 
-	void iterativeeDFS(int s) {
+	void iterativeDFS(int s) {
 		Vector<Boolean> visited = new Vector<>(V);
 		for (int i = 0; i < V; i++) {
 			visited.add(false);
@@ -53,7 +54,22 @@ class Graph {
 	}
 
 	void queueBFS(int s) {
-//		TODO: implement this!
+		boolean[] visited = new boolean[V];
+		LinkedList<Integer> queue = new LinkedList<>();
+		visited[s] = true;
+		queue.add(s);
+
+		while (queue.size() != 0) {
+			s = queue.poll();
+			Iterator<Integer> i = lists[s].listIterator();
+			while (i.hasNext()) {
+				int n = i.next();
+				if (!visited[n]) {
+					visited[n] = true;
+					queue.add(n);
+				}
+			}
+		}
 	}
 
 	void stackBFS(int s) {
